f8969683f50187b2ec83def32274220325d6fca3
zodiac0296 <93139540+zodiac0296@users.noreply.github.com>
2022-08-12 19:03:05 +0600

    My Project

---
+++ app/src/main/java/com/example/receiptproject/ui/profile/ProfileFragment.kt
@@ -0,0 +1,24 @@
+package com.example.receiptproject.ui.profile
+
+import android.os.Binder
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.receiptproject.R
+import com.example.receiptproject.databinding.FragmentProfileBinding
+
+class ProfileFragment : Fragment() {
+
+
+    lateinit var binding: FragmentProfileBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentProfileBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+}
